e1d3ffd2d7e649dd5523ba0b12de4a7fe689b1cb
Harenderchhoker31 <harenderchhoker31@gmail.com>
2025-09-17 11:09:46 +0530

    Update landing.jsx

--- Nexotrack/src/components/landing.jsx
+++ Nexotrack/src/components/landing.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
-import Home from './home'
+import Home from './Home';
 import Footer from './Footer';
 
 const Landing = () => {
